dd21aead94dd0c55f93808881df750dffda02682
Nadire Gokcehan <nadireg@gmail.com>
2019-12-24 19:19:37 -0500

    added divs for plots

---
+++ app.py
@@ -0,0 +1,15 @@
+# import dependencies
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
