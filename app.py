27e41bdeaf8594f55dbe3a4abeed4da2925a99ca
Reid Powell <reid.e.powell@gmail.com>
2019-12-29 19:43:44 -0500

    added API route to Flask & jsonified SQL data

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+import psycopg2
+import sys
 
+import simplejson
 
 
 app = Flask(__name__)
@@ -51,6 +54,18 @@ def map():
 def plots():
 
     return render_template("plots.html")
+
+
+
+@app.route("/data")
+def jsonify_data():
+
+    con = psycopg2.connect("host='ec2-107-21-214-26.compute-1.amazonaws.com' dbname='d2h03dui2ottoc' user='zofuoyzecnqaev' password='d3eb573f21fa96d331efb6cdbd41a1de7720a22a36aafd44c90e728bbefabf76'")  
+    cur = con.cursor()
+    cur.execute("""select * from green_energy""")
+    data = [col for col in cur]
+    cur.close()
+    return jsonify(data)
 
 
 
---
+++ config.py
@@ -0,0 +1,2 @@
+HEROKU_PG_URI = "postgres://zofuoyzecnqaev:d3eb573f21fa96d331efb6cdbd41a1de7720a22a36aafd44c90e728bbefabf76@ec2-107-21-214-26.compute-1.amazonaws.com:5432/d2h03dui2ottoc"
+PASSWORD = 'd3eb573f21fa96d331efb6cdbd41a1de7720a22a36aafd44c90e728bbefabf76'
