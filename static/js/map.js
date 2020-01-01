7902e358fe42b00eb5e582e5e154833b786e6876
Reid Powell <reid.e.powell@gmail.com>
2019-12-31 22:21:10 -0500

    Added choropleth State Lines

--- static/js/map.js
+++ static/js/map.js
@@ -8,16 +8,14 @@ var myMap = L.map("map", {
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
 attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
 maxZoom: 18,
-id: "mapbox.streets",
+id: "mapbox.light",
 accessToken: API_KEY
 }).addTo(myMap);
 
-var data_url = "/api/energy_comparison"
-// grab the data with d3
-d3.json(data_url, function(data){
     
-    console.log(data.state)
 
-})
+L.geoJson(statesData).addTo(myMap);
+
+
 
 
