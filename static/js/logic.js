843e3c98d229fad701326ae02bacb796f03ab9d9
Nadire Gokcehan <nadireg@gmail.com>
2020-01-01 20:50:59 -0500

    updated choropleth map

---
+++ static/js/logic.js
@@ -0,0 +1,11 @@
+
+var url = "/api/green_energy";
+
+d3.json(url, function(data){
+    console.log(data)
+
+    
+})
+
+
+
--- static/js/map.js
+++ static/js/map.js
@@ -1,23 +1,81 @@
-// Creating map object
-var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
-});
+// create our base map
+var mapboxAccessToken = API_KEY;
+var map = L.map('map').setView([37.8, -96], 5);
 
-// Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-maxZoom: 18,
-id: "mapbox.streets",
-accessToken: API_KEY
-}).addTo(myMap);
+// add light tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
+    id: 'mapbox/light-v9',
+    attribution: "UNC Bootcamp"
+}).addTo(map);
 
-var data_url = "/api/energy_comparison"
+//  add states data to the map
+L.geoJson(statesData).addTo(map);
+
+//  assign our data route to a variable
+var data_url = "/api/energy_comparison";
+
 // grab the data with d3
 d3.json(data_url, function(data){
-    
-    console.log(data.state)
 
-})
+    // loop through our data and join with states data
+    for(var i = 0; i < data.state.length; i++) {
+        for(var j = 0; j < data.state.length; j++) {
+            if(data.state[i] === statesData.features[j].properties.name) {
+                statesData.features[j].properties.energy_difference = data.energy_difference[i];
+                statesData.features[j].properties.renewable_total = data.renewable_total[i];
+                statesData.features[j].properties.rank = data.rank[i];
+                statesData.features[j].properties.total_energy_consumed_gwh = data.total_energy_consumed_gwh[i];
+            }
+        }
+    };
+
+    console.log('statesData:', statesData.features);
+
+    //  create a function to grap popup info
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h5>  " + (feature.properties.name)+ "</h5><p><strong>Renewable Energy: </strong>" + feature.properties.renewable_total +
+          "</p><strong>Total Consumed Energy: </strong>" + (feature.properties.total_energy_consumed_gwh) + "</p>" +
+          "<p><strong>Consumed Energy Rank: </strong>" + (feature.properties.rank)) + "</p>"
+    };
+
+    //  grab energy difference data and find min and max value to create color scale
+    var ed = data.energy_difference;
+    var min = Math.min.apply(null, ed);
+    var max = Math.max.apply(null, ed);
+
+    console.log(ed)
+
+    console.log('min, max:', min, max);
+
+    // create function that assign colors
+    function getColor(d) {
+        return d < 0  ? "#f7fcf5":
+               d < 1000000 ? "#e5f5e0":
+               d < 5000000 ? "#c7e9c0":
+               d < 10000000 ? "#a1d99b":
+               d < 20000000 ? "#74c476":
+               d < 40000000 ? "#41ab5d":
+               d < 50000000 ? "#238b45":
+                              "#005a32"                                   
+    }
+
+    // create style function 
+    function style(feature) {
+        return {
+            fillColor: getColor(feature.properties.energy_difference),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        };
+    }
+    
+    //  add styles and popup to our map
+    L.geoJson(statesData, {style: style, onEachFeature:onEachFeature}).addTo(map);
+
+});
+
+
 
 
