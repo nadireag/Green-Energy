d4e485f21c98b79be8bcd51c019ae6f2e64b03e0
Nadire Gokcehan <nadireg@gmail.com>
2020-01-05 23:07:52 -0500

    added most popular types plot and screen size responsiveness

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,83 +1,6 @@
 // get our url for the data
 var url = "/api/green_energy";
 
-// //  use d3 to select variables for the plot
-// d3.json(url, function(data){
-//     console.log(data)
-//     //  grab the variables necessary for the plots
-//     var energy_consumption = data.energy_consumption;
-//     var population = data.population;
-//     var state = data.state
-//     var rural_solar = data.rural_solar;
-//     var urban_solar = data.urban_solar;
-
-//     console.log(energy_consumption)
-//     console.log(population)
-
-//     //  create the trace
-//     var trace1 = {
-//         x: state,
-//         y: energy_consumption,
-//         type: "bar",
-//         name: "Energy Consumption"
-//     };
-    
-//     //  create the data array
-//     var data = [trace1];
-    
-//     // create layout
-//     var layout = {
-//         title: "Total Energy Consumption per Capita (million Btu)",
-//         xaxis: { 
-//             title : "States",
-//             tickangle: -36
-//         },
-//         yaxis: { title: "Energy Consumption"}
-//     };
-
-//     // create the bar plot
-//     Plotly.newPlot("plot4", data, layout)
-
-
-//     ////////////////////////////////////////////////
-//     //  create the traces for the scatter plot
-//     var trace2 = {
-//         x: state,
-//         y: urban_solar,
-//         mode: "markers",
-//         type: "scatter",
-//         name: "Urban Solar",
-//         marker: {size: 12}
-//     };
-
-//     var trace3 = {
-//         x: state,
-//         y: rural_solar,
-//         mode:"markers",
-//         type: "scatter",
-//         name: "Rural Solar",
-//         marker: {size: 12}
-//     };
-
-//     // create data1 array
-//     var data1 = [trace2, trace3]
-
-//     var layout1 = {
-//         title: "Rural vs Urban Solar Energy",
-//         xaxis: { 
-//                 title : "States",
-//                 tickangle: -36,
-//             },
-//         yaxis: { title: "Solar"}
-//     };
-
-//     // create the scatter plot
-//     Plotly.newPlot("plot3", data1, layout1)
- 
-// });
-
-// ////////////////////////////////////////////////////
-
 // Select data and get value variale for the plot
 d3.json(url, function(data) {
     document.getElementById("selDataset2").addEventListener("change", function() {
@@ -91,51 +14,124 @@ d3.json(url, function(data) {
     //  create keys variable
     var keys = Object.keys(data);
 
-    // get the data for the dropdwown menu
+    // select the data for the dropdwown menu
     keys.forEach(function(d) {
-        if (d !== "state" && d !== "id" && d!="rank"){
+        if (d !== "state" && d !== "id" && d!="rank" && d!="population"){
             dropdown.append("option").text(d).property("value");
         }       
     });
     //  display the plot for the second index value
     getPlot(keys[1], data);
+
+    //  create a function that creates the plot
+    function getPlot(value, data) {
+        
+        //  create the trace variable
+        var trace = {
+            x: data.state,
+            y: data[value],
+            mode:"markers",
+            type: "scatter",
+            name: value,
+            marker: {
+                size: 14,
+                color: 'rgb(44, 160, 101)',
+            },      
+        };
+
+        // create the layout
+        var layout = {
+            title: "<b>Renewable Energy Comparison for States</b>",
+            font:{
+                size: 12
+            },
+            // showlegend: true,
+            xaxis: { 
+                title : "States",
+                tickangle: -40,
+                dtick:1
+            },
+            margin: {
+                l: 50,
+                r: 50,
+                b: 100,
+                t: 100,
+                pad: 5
+            },
+            legend:{
+                bgcolor: "#E2E2E2 "
+            },
+            yaxis:{
+                title: "unit (Gwhs)"
+            }
+        };
+        // create the plot
+        Plotly.newPlot("plot3", [trace], layout, {responsive: true});
+    }
 });
 
-//  create a function that creates the plot
-function getPlot(value, data) {
-    
-    //  create the trace variable
-    var trace = {
-        x: data.state,
-        y: data[value],
-        mode:"markers",
-        type: "scatter",
-        name: "Renewable Energy",
-        marker: {
-            size: 15,
-            color: 'rgb(44, 160, 101)',
-        }       
+//  get the data with url
+d3.json(url, function(data){
+   
+    //  create renewable variable to store total values for each renewable type
+    var renewable = [];
+
+    //  assign data values to a varibale
+    var data_values = Object.values(data)
+    // console.log(data_values)
+
+
+    // loop through array of arrays to get total varibale value for each renewable energy type
+    for (var i=0; i< data_values.length; i++){
+        var total = 0;
+        if ( i!= 14 && i!= 7 && i!= 11 && i!=10 && i!=4){
+            for (var j=0; j< data_values[i].length; j ++){
+                total += data_values[i][j]
+            }
+            // push the totals in the renewable array
+            renewable.push(total)
+        }
     };
 
-    // create the layout
-    var layout = {
-        title: "Renewable Energy Comparison for States",
-        font:{
-            size: 12
-        },
-        xaxis: { 
-            title : "States",
-            tickangle: -45,
-            dtick:0.85
-        },
-        margin: {
-            l: 50,
-            r: 50,
-            b: 100,
-            t: 100,
-            pad: 5
-          }
+    //  create renevable_names array for the renewable energy names
+    var renewable_names = [];
+    var names = Object.keys(data);
+    
+    // loop through the Object.keys values to grab desired renewable energy names
+    for (var i=0; i < names.length; i ++){
+        if (names[i] != "energy_consumption" && names[i] != "rank" && names[i] !="id" &&
+         names[i] != "state" && names[i] != "population"){
+             renewable_names.push(names[i])
+        }
     };
-    // create the plot
-    Plotly.newPlot("plot3", [trace], layout);
-}
+
+    //  create trace2 and data_total variables for the plot
+    var trace2 = {
+        x: renewable_names,
+        y: renewable,
+        type: "bar",
+        name:"Unit (Gwhs)",
+        marker: {
+            color: "#fd8d3c"
+        }
+    };
+
+    var data_total = [trace2];
+
+    // create layout variable for the plot's styling
+    var layout = {
+        title:"<b>Most Popular Renewable Energy Types</b>",
+        xaxis:{
+            title:"Renewable Energy Types",
+            tickangle: -20
+        },
+        showlegend: true,
+        legend:{
+            bgcolor: "#E2E2E2 "
+        }
+    }
+
+    // create the new plot
+    Plotly.newPlot("plot4", data_total, layout, {responsive: true})
+
+});
